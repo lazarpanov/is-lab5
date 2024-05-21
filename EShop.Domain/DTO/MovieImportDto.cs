31ea1d87835bcbc0461d49e7a4e73aa7ef368dae
lazarpanov <116360184+lazarpanov@users.noreply.github.com>
2024-05-21 03:40:57 +0200

    Add project files.

---
+++ EShop.Domain/DTO/MovieImportDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.DTO
+{
+    public class MovieImportDto
+    {
+        public string MovieName { get; set; }
+        
+        public string MovieDescription { get; set; }
+        
+        public string MovieImage { get; set; }
+        
+        public double Rating { get; set; }
+    }
+}
---
+++ EShop.Web/Controllers/API/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using EShop.Domain.Domain;
+using EShop.Domain.DTO;
+using EShop.Domain.Identity;
+using EShop.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Movie_App.Service.Implementation;
+using Movie_App.Service.Interface;
+
+namespace EShop.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly IMovieService movieService;
+        public AdminController(IOrderService orderService, IMovieService movieService)
+        {
+            _orderService = orderService;
+            this.movieService = movieService;
+        }
+
+        [HttpGet]
+        public List<Order> Index()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+        [HttpGet("[action]")]
+        public List<Order> GetAllOrders()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+        [HttpPost("[action]")]
+        public Order GetDetailsForOrder(BaseEntity model)
+        {
+            return _orderService.GetDetailsForOrder(model);
+        }
+
+        [HttpPost("[action]")]
+        public bool ImportMovies(List<MovieImportDto> model)
+        {
+            bool status = true;
+
+            foreach (var item in model)
+            {
+                var movie = new Movie
+                {
+                    MovieName = item.MovieName,
+                    MovieDescription = item.MovieDescription,
+                    MovieImage = item.MovieImage,
+                    Rating = item.Rating
+                };
+
+                
+                movieService.CreateNewMovie(movie);
+
+            }
+            return status;
+        }
+    }
+}
